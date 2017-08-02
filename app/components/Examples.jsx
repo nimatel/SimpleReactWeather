bd6662cc74294a7b894bfc19eaa2ff6277fd3c2a
Ivan Zupancic <ivicazup@gmail.com>
2017-08-02 14:17:35 +0200

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,7 +10,11 @@ var React = require('react')
 
 var Examples = (props) => {
         return (
-            <h3>Examples Component</h3>
+
+            <div>
+                <h3>Examples Component</h3>
+                <p>Welcome to examples page!</p>
+            </div>
         )
 };
 
