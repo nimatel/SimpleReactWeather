90d4b6bec418a0c8fd39be36f8ef4eebc2c3f601
Ivan Zupancic <ivicazup@gmail.com>
2017-08-02 15:05:22 +0200

    renamed stuff in about page

--- public/bundle.js
+++ public/bundle.js
@@ -26624,9 +26624,18 @@
 
 	var About = function About(props) {
 	    return React.createElement(
-	        'h3',
+	        'div',
 	        null,
-	        'About Component'
+	        React.createElement(
+	            'h3',
+	            null,
+	            'About'
+	        ),
+	        React.createElement(
+	            'p',
+	            null,
+	            'welcome to the about page!'
+	        )
 	    );
 	};
 
