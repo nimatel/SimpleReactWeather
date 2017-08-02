a8b8dbc2da409c50f8a1fb747a1dfa9ccdbdfaa9
Ivan Zupancic <ivicazup@gmail.com>
2017-08-02 15:03:53 +0200

    renamed stuff in about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,8 +10,10 @@ var React = require('react')
 
 var About = (props) => {
         return (
-            <h3>About</h3>
-            <p>welcome to the about page!</p>
+            <div>
+                <h3>About</h3>
+                <p>welcome to the about page!</p>
+            </div>
         )
 };
 
