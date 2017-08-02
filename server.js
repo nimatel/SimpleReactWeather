c40e97b00a6e7e9bc54c20c32d10a3749d7a8127
Ivan Zupancic <ivicazup@gmail.com>
2017-08-02 15:09:35 +0200

    changed localhost problem

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(function(req, res, next){
-    if (req.headers['x-forwarded-proto'] === 'http'){
+    if (req.headers['x-forwarded-proto'] === 'https'){
+        res.redirect('http://' + req.hostname + req.url);
         next();
     }else{
-            res.redirect('http://' + req.hostname + req.url);
+            next();
         }
-    
+
 });
 
 app.use(express.static('public'));
